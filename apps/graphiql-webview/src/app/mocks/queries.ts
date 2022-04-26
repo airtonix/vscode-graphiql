7cc9456067218bad2b8fdb592a15e786f755d9c6
Zenobius Jiricek <airtonix@users.noreply.github.com>
2022-04-26 23:40:54 +0930

    fix typechecks

--- apps/graphiql-webview/src/app/mocks/queries.ts
+++ apps/graphiql-webview/src/app/mocks/queries.ts
@@ -3,7 +3,8 @@ import { addResolversToSchema } from '@graphql-tools/schema';
 import { mockServer } from '@graphql-tools/mock';
 
 import { schema } from './schema';
+const executableSchema = buildSchema(schema);
 
-const MockedSchema = addResolversToSchema(buildSchema(schema));
+const MockedSchema = addResolversToSchema(executableSchema);
 
 export const server = mockServer(MockedSchema, {});
