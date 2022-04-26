05b924d089c588f2c956627fead2f163073cf295
Zenobius Jiricek <airtonix@users.noreply.github.com>
2022-04-26 23:34:25 +0930

    linting

--- apps/graphiql-webview/src/app/mocks/queries.ts
+++ apps/graphiql-webview/src/app/mocks/queries.ts
@@ -1,6 +1,7 @@
 import { buildSchema } from 'graphql';
 import { addResolversToSchema } from '@graphql-tools/schema';
 import { mockServer } from '@graphql-tools/mock';
+
 import { schema } from './schema';
 
 const MockedSchema = addResolversToSchema(buildSchema(schema));
